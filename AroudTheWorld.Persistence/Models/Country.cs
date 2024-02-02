a98c72e20103d5c21c1a45e8965a8dbb0c76ef8b
VladShum <vladsh76@gmail.com>
2024-02-02 22:20:50 +0100

    Add fields into Country class

--- AroudTheWorld.Persistence/Models/Country.cs
+++ AroudTheWorld.Persistence/Models/Country.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace AroudTheWorld.Persistence.Models
 {
-    internal class Country
+    public class Country
     {
+        [Key]
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double YCoordinate { get; set; }
+        public double XCoordinate { get; set; }
+        public string CapitalId { get; set; }
+
     }
 }
