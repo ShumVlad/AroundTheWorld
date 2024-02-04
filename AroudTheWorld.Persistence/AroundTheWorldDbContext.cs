c7e1335fdca4d32d23bbf2133bcb4c5861ed32ed
VladShum <vladsh76@gmail.com>
2024-02-04 16:36:10 +0100

    Add AroundTheWorldDbContext class

---
+++ AroudTheWorld.Persistence/AroundTheWorldDbContext.cs
@@ -0,0 +1,26 @@
+﻿using AroudTheWorld.Persistence.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AroudTheWorld.Persistence
+{
+    public class AroundTheWorldDbContext : IdentityDbContext<IdentityUser>
+    {
+        public DbSet<City> Cities { get; set; }
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Place> Places { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        public AroundTheWorldDbContext(DbContextOptions<AroundTheWorldDbContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+            //ApplicationDbContext.Database.Migrate();
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
