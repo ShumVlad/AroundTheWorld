e59393ca5fd3035ddd0bc6952d6d12cbe6ca4525
VladShum <vladsh76@gmail.com>
2024-02-04 15:28:37 +0100

    Add Post class

--- AroundTheWorld/Controllers/IdentityController.cs
+++ AroundTheWorld/Controllers/IdentityController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AroundTheWorld.Controllers
@@ -7,5 +8,15 @@ namespace AroundTheWorld.Controllers
     [ApiController]
     public class IdentityController : ControllerBase
     {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IConfiguration _configuration;
+
+        public IdentityController(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager, IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _configuration = configuration;
+        }
     }
 }
