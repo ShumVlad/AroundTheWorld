d3aa4b107be38ffd203581349110c7c465f8f3e9
VladShum <vladsh76@gmail.com>
2024-02-07 16:21:55 +0100

    Add UnitOfWork class and IRepository interface

--- AroudTheWorld.Persistence/Models/Place.cs
+++ AroudTheWorld.Persistence/Models/Place.cs
@@ -9,6 +9,7 @@ namespace AroudTheWorld.Persistence.Models
         public string Name { get; set; }
         public string Description { get; set; }
         public string Address { get; set; }
+        public string Type { get; set; }
         public string CityId { get; set; }
     }
 }
---
+++ AroudTheWorld.Persistence/Repositories/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AroudTheWorld.Persistence.Repositories
+{
+    public interface IRepository<T>
+    {
+        Task Delete(string id);
+
+        Task Update(T entity);
+
+        Task Add(T entity);
+
+        T Get(string id);
+
+        List<T> GetAll();
+    }
+}
