3ebc8259b464f1cd43dd27869ee2d408c70fc3a7
vlads <vlads@VLAD>
2024-05-06 00:00:08 +0200

    Changed UnitOfWork class

--- AroundTheWorld_Backend/UnitOfWork.cs
+++ AroundTheWorld_Backend/UnitOfWork.cs
@@ -10,18 +10,25 @@ namespace AroundTheWorld_Backend
     internal class UnitOfWork
     {
         private readonly AroundTheWorldDbContext context;
-        public readonly IRepository<City> CityRepository;
-        public readonly IRepository<Country> CountryRepository;
+        public readonly IRepository<Group> GroupRepository;
         public readonly IRepository<ApplicationUser> UserRepository;
         public readonly IRepository<Location> PlaceRepository;
-        public readonly IRepository<Post> PostRepository;
-        public UnitOfWork(IRepository<City> cityRepository, IRepository<Country> countryRepository, IRepository<Location> placeRepository,
-            IRepository<ApplicationUser> userRepository, IRepository<Post> postRepository)
+        public readonly IRepository<Media> MediaRepository;
+        public readonly IRepository<Route> RouteRepository;
+        public readonly IRepository<Sensor> SensorRepository;
+        public readonly IRepository<Review> ReviewRepository;
+        public readonly IRepository<Position> PositionRepository;
+        public UnitOfWork(IRepository<Group> groupRepository, IRepository<Media> mediaRepository, IRepository<Location> placeRepository,
+            IRepository<ApplicationUser> userRepository, IRepository<Route> routeRepository, IRepository<Sensor> sensorRepository,
+            IRepository<Review> reviewRepository, IRepository<Position> positionRepository)
         {
             PlaceRepository = placeRepository;
-            CountryRepository = countryRepository;
-            CityRepository = cityRepository;
-            PostRepository = postRepository;
+            PositionRepository = positionRepository;
+            SensorRepository = sensorRepository;
+            GroupRepository = groupRepository;
+            ReviewRepository = reviewRepository;
+            RouteRepository = routeRepository;
+            MediaRepository = mediaRepository;
             UserRepository = userRepository;
             this.context = context;
         }
