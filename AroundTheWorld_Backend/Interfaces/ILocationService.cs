81ba072a62813e4e2b4a7aed399378d096e64e7d
vlads <vlads@VLAD>
2024-05-13 18:46:12 +0200

    Added ILocationService interface

---
+++ AroundTheWorld_Backend/Interfaces/ILocationService.cs
@@ -0,0 +1,18 @@
+﻿using AroundTheWorld_Persistence.Models;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AroundTheWorld_Backend.Interfaces
+{
+    public interface ILocationService
+    {
+        Task<string> Create(Location location);
+        Task<string> Delete(string id);
+        Location Get(string id);
+        Location Update(Location location);
+    }
+}
