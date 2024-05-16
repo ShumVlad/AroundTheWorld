fd4043f64448cfb7dad8ad4e8325fed5330f4388
vlads <vlads@VLAD>
2024-05-16 11:49:23 +0200

    Added RouteDTO

---
+++ AroundTheWorld_Backend/DTOs/RouteDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AroundTheWorld_Backend.DTOs
+{
+    public interface RouteDTO
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
