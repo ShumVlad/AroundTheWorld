204f573c1724961e7aff81c4046552dc7fcd63bd
vlads <vlads@VLAD>
2024-05-16 11:48:20 +0200

    Added IRouteService interface

---
+++ AroundTheWorld/Controllers/RouteController.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AroundTheWorld.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RouteController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+    }
+}
---
+++ AroundTheWorld_Backend/Interfaces/IRouteService.cs
@@ -0,0 +1,11 @@
+﻿using AroundTheWorld_Persistence.Models;
+
+namespace AroundTheWorld_Backend.Interfaces
+{
+    public interface IRouteService
+    {
+        Task<bool> Delete(string id);
+        Location Get(string id);
+        Task<bool> Update(Route route);
+    }
+}
