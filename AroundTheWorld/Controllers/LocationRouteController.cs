2e80e0b4a11c1f0a453e73a5368e6d05f719c889
vlads <vlads@VLAD>
2024-05-18 15:40:13 +0200

    Added Create query to LocationRouteController

--- AroundTheWorld/Controllers/LocationRouteController.cs
+++ AroundTheWorld/Controllers/LocationRouteController.cs
@@ -1,4 +1,6 @@
-﻿using AutoMapper;
+﻿using AroundTheWorld_Backend.Services;
+using AroundTheWorld_Persistence.Models;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AroundTheWorld.Controllers
@@ -7,6 +9,22 @@ namespace AroundTheWorld.Controllers
     [ApiController]
     public class LocationRouteController : Controller
     {
-        private readonly IMapper _mapper;
+        private readonly LocationRouteService _locationRouteService;
+
+        LocationRouteController(LocationRouteService locationRouteService)
+        {
+            _locationRouteService = locationRouteService;
+        }
+        [HttpPost]
+        [Route("Create")]
+        public async Task<bool> Create(LocationRoute locationRoute)
+        {
+            if(locationRoute == null)
+            {
+                throw new ArgumentNullException(nameof(locationRoute));
+            }
+            var result = await _locationRouteService.CreateLocationRoute(locationRoute);
+            return result;
+        }
     }
 }
---
+++ AroundTheWorld_Backend/Services/LocationRouteService.cs
@@ -0,0 +1,30 @@
+﻿
+
+using AroundTheWorld_Persistence.Models;
+using AutoMapper;
+
+namespace AroundTheWorld_Backend.Services
+{
+    public class LocationRouteService
+    {
+        private UnitOfWork _unitOfWork;
+        private Mapper _mapper;
+
+        LocationRouteService(UnitOfWork unitOfWork, Mapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<bool> CreateLocationRoute(LocationRoute locationRoute)
+        {
+            if (locationRoute == null)
+            {
+                throw new ArgumentNullException(nameof(locationRoute));
+            }
+            locationRoute.Id = Guid.NewGuid().ToString();
+            await _unitOfWork.LocationRouteRepository.Add(locationRoute);
+            _unitOfWork.Save();
+            return true;
+        }
+    }
+}
