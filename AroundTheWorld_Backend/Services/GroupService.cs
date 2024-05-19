717092c76b245f4dbc33e9ee395d7aad0a9a4c17
vlads <vlads@VLAD>
2024-05-19 21:02:29 +0200

    Added Update and Get methods to GroupService

--- AroundTheWorld_Backend/Services/GroupService.cs
+++ AroundTheWorld_Backend/Services/GroupService.cs
@@ -1,12 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+
+using AroundTheWorld_Persistence.Models;
 
 namespace AroundTheWorld_Backend.Services
 {
-    internal class GroupService
+    public class GroupService
     {
+        private UnitOfWork _unitOfWork;
+
+        public GroupService(UnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<bool> Add(Group group)
+        {
+            await _unitOfWork.GroupRepository.Add(group);
+            _unitOfWork.Save();
+            return true;
+        }
+
+        public async Task<bool> Delete(string id)
+        {
+            await _unitOfWork.GroupRepository.Delete(id);
+            _unitOfWork.Save();
+            return true;
+        }
+
+        public async Task<bool> Update(Group group)
+        {
+            await _unitOfWork.GroupRepository.Update(group);
+            _unitOfWork.Save();
+            return true;
+        }
+
+        public Group Get(string id) 
+        {
+            Group result = _unitOfWork.GroupRepository.Get(id);
+            return result;
+        }
     }
 }
