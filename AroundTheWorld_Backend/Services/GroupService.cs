e15618a21ba246494747143adee9feb1ade3d68a
vlads <vlads@VLAD>
2024-05-19 20:59:21 +0200

    Added Add and Delete methods to GroupService

---
+++ AroundTheWorld_Backend/Services/GroupService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AroundTheWorld_Backend.Services
+{
+    internal class GroupService
+    {
+    }
+}
