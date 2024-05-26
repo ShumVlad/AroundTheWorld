20868c77289cfc24b9f9bf970db0028af0e3948e
vlads <vlads@VLAD>
2024-05-26 20:58:55 +0200

    Added RegisterWorker component

---
+++ aroundtheworld_website/src/Components/RegisterWorker/RegisterWorker.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+const RegisterWorker = React.forwardRef((props, ref) => {
+    if (!props.data) {
+        return null;
+    }
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    
+    const hadleSave =(e) => {
+        e.preventDefault();
+      
+        const url ='https://localhost:7044/api/Identity/Registration'; 
+        const data = {
+          Username: name,
+          Email: email,
+          Password: password,
+          CompanyId: props.data.CompanyId
+        }
+        
+        axios.post(url, data)
+        .then((result) => {
+          const dt = result.data;
+          navigate("/")
+        })
+        .catch((error) =>{
+          console.log(error);
+        })
+    }
+
+    return (
+        <div ref={ref}>
+            <div className='aroundTheWorld__registerWorker'>
+                <input type="name" placeholder="User Name" onChange={(e) => setName(e.target.value)}></input>
+                <input type="email" placeholder="Email Address" onChange={(e) => setEmail(e.target.value)}></input>
+                <input type="password" placeholder="password" onChange={(e) => setPassword(e.target.value)}></input>
+                <button type="button"  onClick={(e) => hadleSave(e)}>Register Worker</button>
+            </div>
+        </div>
+    );
+});
+
+export default RegisterWorker;
